69c57c15907d5f74322dd3aec3ae5998a390d5e6
Pablo Bonilla <pbonilla29@hotmail.com>
2019-06-08 00:03:55 -0600

    Add código fuente de la implementación del API de Cloudinary

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio3
+{
+    class Program
+    {
+        public static Cloudinary cloudinary;
+
+        public const string CLOUD_NAME = "cloud_name";
+        public const string API_KEY = "api_key";
+        public const string API_SECRET = "api_secret";
+   
+        static void Main(string[] args)
+        {
+            Account account = new Account(CLOUD_NAME, API_KEY, API_SECRET);
+            cloudinary = new Cloudinary(account);
+
+            Console.WriteLine("[Please specify image path]");
+            string imagePath = Console.ReadLine();
+            uploadImage(imagePath);
+            Console.ReadLine();
+        }
+
+        public static void uploadImage(string imagePath)
+        {
+            try
+            {
+                var uploadParams = new ImageUploadParams()
+                {
+                    File = new FileDescription(imagePath)
+                };
+
+                var uploadResult = cloudinary.Upload(uploadParams);
+                Console.WriteLine("[Image was uploaded successfully]");
+            } catch (Exception e)
+            {
+                Console.WriteLine(e.Message); 
+            }         
+        }
+    }
+}
